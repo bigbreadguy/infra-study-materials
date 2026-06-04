788d44caaeeaef7afce0830fa3a390e5065d7c95
kenny <kenny@kennyui-MacBookPro.local>
2026-06-04 17:16:43 +0900

    chore: add mongo connection test dag

---
+++ dags/mongo-connection-test.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+# pyrefly: ignore [missing-import]
+from airflow.sdk import DAG, task
+# pyrefly: ignore [missing-import]
+from airflow.models import Variable
+# pyrefly: ignore [missing-import]
+from airflow.providers.standard.operators.bash import BashOperator
+# pyrefly: ignore [missing-import]
+from airflow.providers.mongo.hooks.mongo import MongoHook
+
+mongo_conn_id = Variable.get("mongo_conn_id", default_var="mongo-default-connection")
+
+# A Dag represents a workflow, a collection of tasks
+with DAG(
+    dag_id="mongo-connection-test",
+    start_date=datetime(2022, 1, 1),
+    schedule=None
+) as dag:
+    # Tasks are represented as operators
+    init = BashOperator(task_id="init", bash_command="echo 'Start connection test'")
+
+    @task()
+    def ping_mongo():
+        with MongoHook(mongo_conn_id=mongo_conn_id) as hook:
+            response = hook.get_conn().admin.command("ping")
+        assert response["ok"] == 1.0
+        print(response)
+
+    # Set dependencies between tasks
+    init >> ping_mongo()
